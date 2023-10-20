8d5321ce5778fa64573f2b86bcd791f7c326b8c7
GopindaAditya <komanggovinda59@gmail.com>
2023-10-20 08:28:07 +0700

    add peminjaman routes

---
+++ routes/peminjaman.js
@@ -0,0 +1,5 @@
+var express = require('express');
+var router = express.Router();
+const Validator = require('fastest-validator');
+const v = new Validator();
+const {Ruangan} = require('../models');
